1735c5ddaa91590e4adf9dced6fdf13ced9e7185
HyCraftHD <hycrafthd@live.de>
2018-09-29 20:52:44 +0200

    Fixed proxy error

--- src/main/java/info/u_team/overworld_mirror/OverworldMirrorConstants.java
+++ src/main/java/info/u_team/overworld_mirror/OverworldMirrorConstants.java
@@ -11,8 +11,8 @@ public class OverworldMirrorConstants {
 	public static final String DEPENDENCIES = "required:forge@[14.23.4.2705,);required-after:uteamcore@[2.0.0.81,);";
 	public static final String UPDATEURL = "https://api.u-team.info/update/overworldmirror.json";
 	
-	public static final String COMMONPROXY = "info.u_team.overworldmirror.proxy.CommonProxy";
-	public static final String CLIENTPROXY = "info.u_team.overworldmirror.proxy.ClientProxy";
+	public static final String COMMONPROXY = "info.u_team.overworld_mirror.proxy.CommonProxy";
+	public static final String CLIENTPROXY = "info.u_team.overworld_mirror.proxy.ClientProxy";
 	
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
 	
