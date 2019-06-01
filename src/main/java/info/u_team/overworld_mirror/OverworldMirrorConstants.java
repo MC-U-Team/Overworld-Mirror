d9f2f0bf51b67bffaa34a83b75ef4951e4a5d765
HyCraftHD <hycrafthd@live.de>
2019-06-01 14:56:53 +0200

    Fix dependencies

--- src/main/java/info/u_team/overworld_mirror/OverworldMirrorConstants.java
+++ src/main/java/info/u_team/overworld_mirror/OverworldMirrorConstants.java
@@ -8,7 +8,7 @@ public class OverworldMirrorConstants {
 	public static final String NAME = "Overworld Mirror";
 	public static final String VERSION = "${version}";
 	public static final String MCVERSION = "${mcversion}";
-	public static final String DEPENDENCIES = "required:forge@[14.23.4.2705,);required-after:uteamcore@[2.0.0.81,);after:morpheus";
+	public static final String DEPENDENCIES = "required:forge@[14.23.5.2768,);required-after:uteamcore@[2.2.4.107,);after:morpheus";
 	public static final String UPDATEURL = "https://api.u-team.info/update/overworldmirror.json";
 	
 	public static final String COMMONPROXY = "info.u_team.overworld_mirror.proxy.CommonProxy";
