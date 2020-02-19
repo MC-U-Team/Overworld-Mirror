27c8d13471a0276ce85353bd6f1e422f3800938f
HyCraftHD <hycrafthd@live.de>
2020-02-19 17:56:58 +0100

    Check if jar is signed now

--- src/main/java/info/u_team/overworld_mirror/OverworldMirrorMod.java
+++ src/main/java/info/u_team/overworld_mirror/OverworldMirrorMod.java
@@ -1,10 +1,15 @@
 package info.u_team.overworld_mirror;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(OverworldMirrorMod.MODID)
 public class OverworldMirrorMod {
 	
 	public static final String MODID = "overworldmirror";
+	
+	public OverworldMirrorMod() {
+		JarSignVerifier.checkSigned(MODID);
+	}
 	
 }
