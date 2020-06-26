ac3c739dd444824b84c0799df30a68c583aa0690
HyCraftHD <hycrafthd@live.de>
2020-06-27 01:54:07 +0200

    Register portal block with the deferred register

--- src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorBlocks.java
+++ src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorBlocks.java
@@ -2,14 +2,15 @@ package info.u_team.overworld_mirror.init;
 
 import info.u_team.overworld_mirror.OverworldMirrorMod;
 import info.u_team.overworld_mirror.block.OverworldMirrorPortalBlock;
-import info.u_team.u_team_core.util.registry.BlockDeferredRegister;
+import info.u_team.u_team_core.util.registry.*;
+import net.minecraft.item.BlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class OverworldMirrorBlocks {
 	
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(OverworldMirrorMod.MODID);
 	
-	public static final OverworldMirrorPortalBlock PORTAL = new OverworldMirrorPortalBlock("portal");
+	public static final BlockRegistryObject<OverworldMirrorPortalBlock, BlockItem> PORTAL = BLOCKS.register("portal", OverworldMirrorPortalBlock::new);
 	
 	public static void register(IEventBus bus) {
 		BLOCKS.register(bus);
