234136c7b9fa65b94b32d9a6340740da8e66e0d6
HyCraftHD <hycrafthd@live.de>
2021-05-01 18:40:02 +0200

    Fix portal creation event

--- src/main/java/info/u_team/overworld_mirror/event/PortalCreationEventHandler.java
+++ src/main/java/info/u_team/overworld_mirror/event/PortalCreationEventHandler.java
@@ -3,8 +3,11 @@ package info.u_team.overworld_mirror.event;
 import info.u_team.overworld_mirror.OverworldMirrorMod;
 import info.u_team.overworld_mirror.portal.PortalManager;
 import net.minecraft.block.FlowerBlock;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -32,8 +35,15 @@ public class PortalCreationEventHandler {
 		}
 		
 		if (PortalManager.trySpawnPortalFromFrame(world, pos)) {
-			LightningBoltEntity lightning = new LightningBoltEntity(world, pos.getX(), pos.getY() + 1, pos.getZ(), true);
-			world.addLightningBolt(lightning);
+			final LightningBoltEntity lightning = EntityType.LIGHTNING_BOLT.create(world);
+			lightning.moveForced(Vector3d.copyCenteredHorizontally(pos.up()));
+			lightning.setEffectOnly(true);
+			
+			if (event.getPlayer() instanceof ServerPlayerEntity) {
+				lightning.setCaster((ServerPlayerEntity) event.getPlayer());
+			}
+			
+			world.addEntity(lightning);
 		}
 	}
 }
