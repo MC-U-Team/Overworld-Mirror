8760cbc08a9e76741c911eccbc1486cdc7fbaa96
HyCraftHD <hycrafthd@live.de>
2021-05-01 18:37:04 +0200

    Register the render types

--- src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorClientConstruct.java
+++ src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorClientConstruct.java
@@ -3,13 +3,14 @@ package info.u_team.overworld_mirror.init;
 import info.u_team.overworld_mirror.OverworldMirrorMod;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.IModConstruct;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = OverworldMirrorMod.MODID, client = true)
 public class OverworldMirrorClientConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
-		
+		BusRegister.registerMod(OverworldMirrorRenderTypes::registerMod);
 	}
 	
 }
--- src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorRenderTypes.java
+++ src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorRenderTypes.java
@@ -2,23 +2,19 @@ package info.u_team.overworld_mirror.init;
 
 import static info.u_team.overworld_mirror.init.OverworldMirrorBlocks.PORTAL;
 
-import info.u_team.overworld_mirror.OverworldMirrorMod;
-import net.minecraft.client.renderer.*;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = OverworldMirrorMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class OverworldMirrorRenderTypes {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
-		// Translucent
-		final RenderType transluctent = RenderType.getTranslucent();
-		
-		RenderTypeLookup.setRenderLayer(PORTAL.get(), transluctent);
+	private static void setup(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(PORTAL.get(), RenderType.getTranslucent());
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(OverworldMirrorRenderTypes::setup);
 	}
 	
 }
