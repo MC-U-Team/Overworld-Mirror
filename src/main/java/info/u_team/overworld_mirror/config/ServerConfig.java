77533cfe814b88227f488df1919f75436a65ff79
HyCraftHD <hycrafthd@live.de>
2021-05-10 12:52:15 +0200

    Update server config

--- src/main/java/info/u_team/overworld_mirror/config/ServerConfig.java
+++ src/main/java/info/u_team/overworld_mirror/config/ServerConfig.java
@@ -20,41 +20,14 @@ public class ServerConfig {
 		return INSTANCE;
 	}
 	
-	public final EnumValue<SeedType> seedType;
-	public final LongValue seedValue;
-	
-	public final ConfigValue<String> generatorType;
-	public final ConfigValue<String> generatorSettings;
-	
-	public final DoubleValue movementFactor;
-	public final DoubleValue portalDistance;
+	public final DoubleValue portalSearchDistanceOverworld;
+	public final DoubleValue portalSearchDistanceOverworldMirror;
 	
 	private ServerConfig(Builder builder) {
-		builder.comment("Seed settings").push("seed");
-		seedType = builder.comment("If you have set this to \"SEED\" then the seedValue value will be treated as new seed, else if its set to \"ADDITION\" the value will be added to the main world seed.").defineEnum("seedType", SeedType.ADDITION);
-		seedValue = builder.comment("The seed value. See seedType for more information.").defineInRange("seedValue", 100_000, Long.MIN_VALUE, Long.MAX_VALUE);
-		builder.pop();
-		builder.comment("Generation settings").push("generator");
-		generatorType = builder.comment("Generator type e.g. default, flat, buffet, ... (See here for a list: https://minecraft.gamepedia.com/World_type)").define("generatorType", "default");
-		generatorSettings = builder.comment("Generator settings for the generator type. The snbt string must be valid json and escaped. For flat worlds you must use the snbt variant like this: ({\"biome\":\"minecraft:plains\",\"layers\":[{\"block\":\"minecraft:bedrock\",\"height\":1},{\"block\":\"minecraft:dirt\",\"height\":2},{\"block\":\"minecraft:grass_block\",\"height\":1}],\"structures\":{\"village\":{}}}) and not the client side preset code (minecraft:bedrock,30*minecraft:dirt,minecraft:grass_block;minecraft:plains;village)").define("generatorSettings", "");
-		builder.pop();
 		builder.comment("Portal settings").push("portal");
-		movementFactor = builder.comment("Movement factor. Like in the nether you move 8 times the block length as in the overworld").defineInRange("movementFactor", 1, 0, 1e10);
-		portalDistance = builder.comment("How many blocks the portal can be from the normal spawn location to not create a new one. The value is not in sqrt cause of lag reduction. 400 = 20 Blocks, 900 = 30 Blocks, etc").defineInRange("portalDistance", 400, 1, 1e20);
+		portalSearchDistanceOverworld = builder.comment("How many blocks the portal can be from the normal spawn location to not create a new one.").defineInRange("portalSearchDistanceOverworld", 30, 1, 1e10);
+		portalSearchDistanceOverworldMirror = builder.comment("How many blocks the portal can be from the normal spawn location to not create a new one.").defineInRange("portalSearchDistanceOverworldMirror", 30, 1, 1e10);
 		builder.pop();
-	}
-	
-	public static enum SeedType {
-		
-		SEED,
-		ADDITION;
-		
-		public long calculateSeed(long seedValue, long worldSeed) {
-			if (this == SEED) {
-				return seedValue;
-			}
-			return worldSeed + seedValue;
-		}
 	}
 	
 }
