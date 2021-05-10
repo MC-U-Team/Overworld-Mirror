2512f77017ef57fe38c5a55ffd612592f8cc953d
HyCraftHD <hycrafthd@live.de>
2021-05-10 12:39:04 +0200

    Only register the portal block and not any item

--- src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorBlocks.java
+++ src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorBlocks.java
@@ -2,15 +2,15 @@ package info.u_team.overworld_mirror.init;
 
 import info.u_team.overworld_mirror.OverworldMirrorMod;
 import info.u_team.overworld_mirror.block.OverworldMirrorPortalBlock;
-import info.u_team.u_team_core.util.registry.*;
-import net.minecraft.item.BlockItem;
+import info.u_team.u_team_core.util.registry.BlockDeferredRegister;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 
 public class OverworldMirrorBlocks {
 	
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(OverworldMirrorMod.MODID);
 	
-	public static final BlockRegistryObject<OverworldMirrorPortalBlock, BlockItem> PORTAL = BLOCKS.register("portal", OverworldMirrorPortalBlock::new);
+	public static final RegistryObject<OverworldMirrorPortalBlock> PORTAL = BLOCKS.registerBlock("portal", OverworldMirrorPortalBlock::new);
 	
 	public static void registerMod(IEventBus bus) {
 		BLOCKS.register(bus);
