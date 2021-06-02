d60e4487abef3d2d84fe4d658c113416385a1f3c
HyCraftHD <hycrafthd@live.de>
2021-06-02 22:30:52 +0200

    Remove item models data gen

--- src/data/java/info/u_team/overworld_mirror/data/OverworldMirrorDataGenerator.java
+++ src/data/java/info/u_team/overworld_mirror/data/OverworldMirrorDataGenerator.java
@@ -1,7 +1,8 @@
 package info.u_team.overworld_mirror.data;
 
 import info.u_team.overworld_mirror.OverworldMirrorMod;
-import info.u_team.overworld_mirror.data.provider.*;
+import info.u_team.overworld_mirror.data.provider.OverworldMirrorBlockStatesProvider;
+import info.u_team.overworld_mirror.data.provider.OverworldMirrorLanguagesProvider;
 import info.u_team.u_team_core.data.GenerationData;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -16,7 +17,6 @@ public class OverworldMirrorDataGenerator {
 		final GenerationData data = new GenerationData(OverworldMirrorMod.MODID, event);
 		if (event.includeClient()) {
 			data.addProvider(OverworldMirrorBlockStatesProvider::new);
-			data.addProvider(OverworldMirrorItemModelsProvider::new);
 			
 			data.addProvider(OverworldMirrorLanguagesProvider::new);
 		}
