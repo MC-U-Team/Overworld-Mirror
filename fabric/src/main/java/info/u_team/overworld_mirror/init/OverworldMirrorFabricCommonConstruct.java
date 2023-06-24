600e42690f11914f44ea4ea37e93bb0ce27b23d0
HyCraftHD <hycrafthd@live.de>
2023-06-24 02:54:52 +0200

    Implement level events for fabric

--- fabric/src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorFabricCommonConstruct.java
+++ fabric/src/main/java/info/u_team/overworld_mirror/init/OverworldMirrorFabricCommonConstruct.java
@@ -2,8 +2,11 @@ package info.u_team.overworld_mirror.init;
 
 import info.u_team.overworld_mirror.OverworldMirrorReference;
 import info.u_team.overworld_mirror.config.FabricCommonConfig;
+import info.u_team.overworld_mirror.event.WorldInfoReplaceEventHandler;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.ModConstruct;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 
 @Construct(modid = OverworldMirrorReference.MODID)
 public class OverworldMirrorFabricCommonConstruct implements ModConstruct {
@@ -12,7 +15,12 @@ public class OverworldMirrorFabricCommonConstruct implements ModConstruct {
 	public void construct() {
 		FabricCommonConfig.getInstance();
 		
-		
+		ServerWorldEvents.LOAD.register((server, level) -> {
+			WorldInfoReplaceEventHandler.onWorldLoad(level);
+		});
+		ServerTickEvents.END_WORLD_TICK.register(level -> {
+			WorldInfoReplaceEventHandler.onWorldTick(level);
+		});
 	}
 	
 }
