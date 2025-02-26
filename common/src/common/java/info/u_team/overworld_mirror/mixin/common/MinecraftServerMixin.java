779442ff6bd4bb637eaa29354aa08a89f949c9ff
HyCraftHD <hycrafthd@live.de>
2025-02-26 20:37:01 +0100

    Add mixin to set seed for overworld mirror dimension (WIP)

---
+++ common/src/common/java/info/u_team/overworld_mirror/mixin/common/MinecraftServerMixin.java
@@ -0,0 +1,34 @@
+package info.u_team.overworld_mirror.mixin.common;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import info.u_team.overworld_mirror.init.OverworldMirrorLevelKeys;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.world.RandomSequences;
+import net.minecraft.world.level.CustomSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.BiomeManager;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.ServerLevelData;
+
+@Mixin(MinecraftServer.class)
+abstract class MinecraftServerMixin {
+	
+	@ModifyArg(method = "Lnet/minecraft/server/MinecraftServer;createLevels(Lnet/minecraft/server/level/progress/ChunkProgressListener;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;Lnet/minecraft/world/level/storage/ServerLevelData;Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/level/dimension/LevelStem;Lnet/minecraft/server/level/progress/ChunkProgressListener;ZJLjava/util/List;ZLnet/minecraft/world/RandomSequences;)V", ordinal = 1), index = 8)
+	private long overworldmirror$createLevels(MinecraftServer server, Executor dispatcher, LevelStorageSource.LevelStorageAccess levelStorageAccess, ServerLevelData serverLevelData, ResourceKey<Level> dimension, LevelStem levelStem, ChunkProgressListener progressListener, boolean isDebug, long biomeZoomSeed, List<CustomSpawner> customSpawners, boolean tickTime, RandomSequences randomSequences) {
+		if (dimension == OverworldMirrorLevelKeys.MIRROR_OVERWORLD) {
+			return BiomeManager.obfuscateSeed(10);
+		} else {
+			return biomeZoomSeed;
+		}
+	}
+	
+}
---
+++ common/src/common/java/info/u_team/overworld_mirror/mixin/common/ServerLevelMixin.java
@@ -0,0 +1,43 @@
+package info.u_team.overworld_mirror.mixin.common;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.mojang.datafixers.DataFixer;
+
+import info.u_team.overworld_mirror.init.OverworldMirrorLevelKeys;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.chunk.storage.ChunkScanAccess;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+
+@Mixin(ServerLevel.class)
+abstract class ServerLevelMixin {
+	
+	@Inject(method = "Lnet/minecraft/server/level/ServerLevel;getSeed()J", at = @At(value = "HEAD"), cancellable = true)
+	private void overworldmirror$getSeed(CallbackInfoReturnable<Long> callbackInfo) {
+		final ServerLevel level = (ServerLevel) (Object) this;
+		if (level.dimension() == OverworldMirrorLevelKeys.MIRROR_OVERWORLD) {
+			callbackInfo.setReturnValue(10L);
+		}
+	}
+	
+	@ModifyArg(method = "Lnet/minecraft/server/level/ServerLevel;<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/StructureCheck;<init>(Lnet/minecraft/world/level/chunk/storage/ChunkScanAccess;Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/RandomState;Lnet/minecraft/world/level/LevelHeightAccessor;Lnet/minecraft/world/level/biome/BiomeSource;JLcom/mojang/datafixers/DataFixer;)V", ordinal = 0), index = 8)
+	private long overworldmirror$init(ChunkScanAccess storageAccess, RegistryAccess registryAccess, StructureTemplateManager structureTemplateManager, ResourceKey<Level> dimension, ChunkGenerator chunkGenerator, RandomState randomState, LevelHeightAccessor heightAccessor, BiomeSource biomeSource, long seed, DataFixer fixerUpper) {
+		if (dimension == OverworldMirrorLevelKeys.MIRROR_OVERWORLD) {
+			return 10;
+		} else {
+			return seed;
+		}
+	}
+	
+}
